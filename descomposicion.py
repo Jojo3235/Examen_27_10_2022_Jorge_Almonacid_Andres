ca55387eb4457f303c5b345341750815b0df7ee4
Jojo3235 <joralmand@gmail.com>
2022-10-27 11:48:37 +0200

    mas pruebas

--- descomposicion.py
+++ descomposicion.py
@@ -1,20 +1,17 @@
 #Ejercicio 5
 
-import sys
-
-
 def pedir_num():
     numero=input("Introduce un número entero positivo: ")
     try:
         numero=int(numero)
     except:
-        print("Se ha de introducir un número entero positivo",file=sys.stderr())
-        sys.exit
+        print("Se ha de introducir un número entero positivo")
     return numero
 
-pedir_num()
+def listificar():
+    numero=pedir_num()
+    lista=list(str(numero))
+    return lista
 
-def lista_num(num):
-    numero=str(num)
-    lista_num=numero.split()
-    return lista_num
+lista_numeros=listificar()
+print(lista_numeros)
